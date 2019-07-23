7448762f81e935d551a692664ee6335c84e6efd3
Mackenzie Bowen <mackenzierbowen12@gmail.com>
2019-07-23 11:17:38 -0400

    added formatTime method, error

--- src/components/Album.js
+++ src/components/Album.js
@@ -128,13 +128,22 @@ class Album extends Component {
         })
     }
 
+    formatTime(duration, song) {
+        if(this.state.isPlaying && this.currentSong === song) {
+            return Math.floor( duration / 60 ) + ':' + Math.floor( duration % 60 );
+            } else {
+                return <span>"-:--"</span>
+            }
+        }
+    
+
     render() {
         
         return (
             <section className="album">
                 <section id="album-info">
                     <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
-                    <div className="albusm-details">
+                    <div className="album-details">
                         <h1 id="album-title">{this.state.album.title}</h1>
                         <h2 className="artist">{this.state.album.artist}</h2>
                         <div id="release-info">{this.state.album.releaseInfo}</div>
